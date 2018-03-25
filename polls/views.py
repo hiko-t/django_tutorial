1a8c305a2c7252b1009a81954a9e6e37afd578a0
Yoshihiko Takahashi <yotti4415@gmail.com>
2018-03-25 21:05:54 +0900

    ショートカットでより簡単にする

--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 
 from .models import Question
 
@@ -9,11 +9,8 @@ def index(request):
     return render(request, 'polls/index.html', context)
 
 def detail(request, question_id):
-    try:
-        question = Question.object.get(pk=question_id)
-    except Qestion.DoesNotExist:
-        raise Http404("Question does not exist")
-    return render(reqest, 'polls/detail.html', {'question': question})
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
 
 def results(request, question_id):
     response = "You're looking at the results of question %s."
